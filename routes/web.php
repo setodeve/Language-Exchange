dd582b7503b9f89f034ad2d5f33156cdd46a3607
setodeve <keinick11@outlook.com>
2022-10-15 08:39:58 +0900

    Delete needless file

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', 'UserClass@welcome');
 Route::get('/', 'App\Http\Controllers\IndexController@index');
